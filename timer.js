43e824b28fe86a8a3903b41d34d3d4c39c2be597
Yaro <12fcv4@gmail.com>
2015-04-05 13:01:50 +0300

    add destroy method

--- timer.js
+++ timer.js
@@ -1,6 +1,8 @@
+"use strict";
+
 function Timer(callback, delay) {
     this.setTimeout(callback, delay);
-	this.is_pause = false;
+    this.is_pause = false;
 }
 
 Timer.prototype.setTimeout = function(callback, delay) {
@@ -21,32 +23,38 @@ Timer.prototype.setTimeout = function(callback, delay) {
 
 Timer.prototype.add = function(delay) {
     if (this.is_finished) {
-	    return;
-	}
-	
-	delay = this.remaining + delay;
-	this.setTimeout(this.callback, delay);
+        return;
+    }
+
+    delay = this.remaining + delay;
+    this.setTimeout(this.callback, delay);
     if (this.is_pause) {
-		this.pause();
-	}
+        this.pause();
+    }
 }
 
 Timer.prototype.pause = function() {
-	if (this.is_finished) {
-	    return;
-	}
-	if (this.timer) {
+    if (this.is_finished) {
+        return;
+    }
+    if (this.timer) {
         window.clearTimeout(this.timer);
     }
-	this.remaining -= window.Date.now() - this.start;
-	this.is_pause = true;
+    this.remaining -= window.Date.now() - this.start;
+    this.is_pause = true;
 };
 
 Timer.prototype.resume = function() {
-	if (this.is_finished) {
-	    return;
-	}
-	this.start = window.Date.now();
-	this.setTimeout(this.callback, this.remaining);
-	this.is_pause = false;
+    if (this.is_finished) {
+        return;
+    }
+    this.start = window.Date.now();
+    this.setTimeout(this.callback, this.remaining);
+    this.is_pause = false;
+};
+
+Timer.prototype.destroy = function() {
+    if (this.timer) {
+        window.clearTimeout(this.timer);
+    }
 };
